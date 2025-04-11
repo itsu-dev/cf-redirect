3867a34a541f403ee267f02bc074abd4fa8aabb1
itsu-dev <itsu020402@gmail.com>
2025-04-12 08:26:14 +0900

    feat: impl

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+type Bindings = {
+	KDB_URL: string
+}
+
+const app = new Hono<{ Bindings: Bindings }>();
+app.use(cors());
+app.get('*', (c) => {
+	return c.redirect(c.env.KDB_URL, 301);
+});
+
+export default app;
